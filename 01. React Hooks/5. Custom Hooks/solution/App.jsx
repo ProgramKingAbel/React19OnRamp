97758ff350a691a280782e637dbb3437efe9a177
Abel Morara <abelmorara254@gmail.com>
2025-02-24 16:16:05 +0300

    feat: add input handler custom hook

---
+++ 01. React Hooks/5. Custom Hooks/solution/App.jsx
@@ -0,0 +1,13 @@
+import ToggleComponent from './components/ToggleComponent';
+import FormComponent from './components/FormComponent';
+
+const App = () => {
+        return (
+                <div>
+                <h1>React Custom Hooks</h1>
+                <ToggleComponent />
+                <FormComponent />
+                </div>
+        );
+}
+export default App;
